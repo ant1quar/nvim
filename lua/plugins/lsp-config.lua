db751e6850ec33c5796848efd1bd39e81aeb9eb6
Krivonos Ivan <ivan.krivonos@crystalblockchain.com>
2024-04-25 09:51:31 +0300

    fix command

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,9 +32,8 @@ return {
       lspconfig.angularls.setup({
         capabilities = capabilities,
       })
-
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gd", "<C-w>s :lua vim.lsp.buf.definition()<CR>", {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
